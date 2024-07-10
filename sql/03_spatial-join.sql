1a413ce2957b3657848a94edd27fa113164ab054
Arvo <64811285+ArvoK@users.noreply.github.com>
2024-07-10 20:59:49 +0200

    spatial join angepasst

--- sql/03_spatial-join.sql
+++ sql/03_spatial-join.sql
@@ -1,26 +1,27 @@
-
 --01
-DROP MATERIALIZED VIEW IF EXISTS daten."brd_sonnenstunden_jahresmittel";
-CREATE MATERIALIZED VIEW daten."brd_sonnenstunden_jahresmittel" AS
+DROP MATERIALIZED VIEW IF EXISTS daten."sonnenstunde-im-jahr-pro-bundesland";
+CREATE MATERIALIZED VIEW daten."sonnenstunde-im-jahr-pro-bundesland" AS
 (
     SELECT
-        CONCAT(dwd_date."id",';',bl."id") as id,
         avg(dwd_av."Sonnenstunden") as "Sonnenstunden",
-        dwd_date."Datum" as "Datum",
+        max(dwd_date."Datum") as "Datum",
         bl."Name" as "Bundesland",
         bl."geom" as geom
     FROM
-        datenbestand.brd_bundeslaender bl
+        daten.brd_bundeslaender bl
 
     INNER JOIN
-        datenbestand."dwd_sonnenstunden_jahresmittel" dwd_av ON ST_INTERSECTS(dwd_av.geom, bl.geom)
+        daten."dwd_sonnenstunden_jahresmittel" dwd_av ON ST_INTERSECTS(dwd_av.geom, bl.geom)
     FULL JOIN
-        datenbestand."dwd_sonnenstunden_jahresmittel" dwd_date ON dwd_av.id = dwd_date.id
+        daten."dwd_sonnenstunden_jahresmittel" dwd_date ON dwd_av.id = dwd_date.id
+	WHERE 
+		bl."Name" IS NOT NULL
     GROUP BY
-        bl."Name", dwd_date."Datum",bl."geom",CONCAT(dwd_date."id",';',bl."id")
+        dwd_date."Datum", bl."Name", bl."geom"
 );
 
 
 --02
-CREATE UNIQUE INDEX idx_brd_sonnenstunden_jahresmittel_id
-  ON daten."brd_sonnenstunden_jahresmittel" (id);
+CREATE INDEX geom_idx
+  ON daten."sonnenstunde-im-jahr-pro-bundesland"
+  USING GIST (geom);
